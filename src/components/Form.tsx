0ccbb03c1090816677ea495b8a49b69e3313fa7f
EquipeWeb02 <equipeweb02@Mac-Filipe.local>
2022-12-06 18:04:30 -0200

    06-12-part2

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,50 +1,53 @@
 import React from "react";
 import "./Form.modules.css";
-import {EnvelopeSimple, SignIn} from 'phosphor-react';
-import  imagem from '../assets/side-image.svg'
+import { EnvelopeSimple, SignIn } from "phosphor-react";
+import imagem from "../assets/side-image.png";
 
 export function Form() {
   return (
+    <>
+      <div className="formLoginDiv1">
+        <div className="formLoginDiv2">
+          <div className="formLogin">
+            <form>
+              <div className="formLoginInitial">
+                <SignIn className="iconSign" />
+                <b>Faça seu login</b>
+                <p>Entre com as suas informações de cadastro.</p>
+              </div>
 
+              <div className="divEmail">
+                <p>E-mail</p>
 
+                <input type="text" placeholder=" Digite seu e-mail"></input>
+              </div>
 
+              <div className="divSenha">
+                <p>Senha</p>
+                <input type="password" placeholder="Digite sua senha"></input>
+              </div>
 
-    <div className="formLoginDiv1">
+              <div className="esqueci">
+                <button type="submit" className="lembrar" />
 
+                <p>Lembre-me</p>
 
-      <div className="formLoginDiv2">
+                <a href="#">Esqueci minha senha</a>
+              </div>
 
-        <div className="formLogin">
-        
-          <form>
-            <SignIn className="iconSign"/>
-            <b>Faça seu login</b>
-            <p>Entre com as suas informações de cadastro.</p>
-            
-            <p>E-mail</p>
-            <input type="text" placeholder="Digite seu e-mail">  </input>
-            <EnvelopeSimple className="iconEvelope"/>
-            <p>Senha</p>
-            <input type="password" placeholder="Digite sua senha">    </input>
-            <button type="submit" className="lembrar">w</button>
-            <div className="esqueci">
-   
-            <p >Lembre-me</p>
+              <div className="footer">
+                <button type="submit" className="entrar">
+                  ENTRAR
+                </button>
 
-            <a href="#">Esqueci minha senha</a>
-            </div>
-            <button type="submit" className="entrar">ENTRAR</button>
-
-            <p>Não tem uma conta? Registre-se</p>
-          </form>
-    
+                <p>Não tem uma conta? Registre-se</p>
+              </div>
+            </form>
+          </div>
         </div>
 
+        <img src={imagem} alt="teste" className="divImg" />
       </div>
-      <img src={imagem} alt="teste"/>
-      
-    </div>
-
-    
-   );
+    </>
+  );
 }
